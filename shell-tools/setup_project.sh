ccc96779cf01c688bf4ed1e171002de9b731e4b9
Koen Klasing <k.klasing@bureaublauwgeel.nl>
2016-07-01 13:59:50 +0200

    Updated shelltools to meet new Helder CMS setup

--- shell-tools/setup_project.sh
+++ shell-tools/setup_project.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-php ../app/console kuma:generate:bundle
-php ../app/console kuma:generate:default-site
-php ../app/console doctrine:database:create
-php ../app/console doctrine:migrations:diff
-php ../app/console doctrine:migrations:migrate
-php ../app/console doctrine:fixtures:load
+php app/console kuma:generate:bundle
+php app/console bbg:generate:default-site
+php app/console doctrine:database:create
+php app/console doctrine:migrations:diff
+php app/console doctrine:migrations:migrate
+php app/console doctrine:fixtures:load
 
 npm install -g bower
 npm install -g gulp
@@ -17,5 +17,5 @@ npm install
 bower install
 gulp build
 
-php ../app/console assets:install --symlink
-php ../app/console assetic:dump
+php app/console assets:install --symlink
+php app/console assetic:dump
