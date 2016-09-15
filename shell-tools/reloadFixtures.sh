485d195b3083c915d2d9347f750a44fc48e118d6
Koen Klasing <k.klasing@bureaublauwgeel.nl>
2016-09-15 20:44:27 +0200

    Fixed reloadFicture shell tool

--- shell-tools/reloadFixtures.sh
+++ shell-tools/reloadFixtures.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-php ../app/console doc:data:drop --force
-php ../app/console doc:data:create
-php ../app/console doc:mi:mi --no-interaction
-php ../app/console doc:schema:up --force
-php ../app/console doc:fix:lo --no-interaction
+php app/console doctrine:database:drop --force
+php app/console doctrine:database:create
+php app/console doctrine:migrations:migrate --no-interaction
+php app/console doctrine:fixtures:load --no-interaction
+
+
 
 
