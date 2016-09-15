1a8eac05f705a0b53293933694bb9127fce0ff05
Koen Klasing <k.klasing@bureaublauwgeel.nl>
2016-09-15 16:40:56 +0200

    Updated shell script for default site setup

--- shell-tools/setup_project.sh
+++ shell-tools/setup_project.sh
@@ -3,20 +3,17 @@
 composer install
 php app/console kuma:generate:bundle
 php app/console bbg:generate:default-site
+php app/console doctrine:database:drop --force
 php app/console doctrine:database:create
 php app/console doctrine:migrations:diff
-php app/console doctrine:migrations:migrate
-php app/console doctrine:fixtures:load
-
-npm install -g bower
-npm install -g gulp
-npm install --save uglify-js uglifycss
-npm shrinkwrap
+php app/console doctrine:migrations:migrate --no-interaction
+php app/console doctrine:fixtures:load --no-interaction
 
 bundle install
-npm install
-bower install
-gulp build
+npm install --save bower gulp uglify-js uglifycss
+npm shrinkwrap
+node_modules/.bin/bower install
+node_modules/.bin/bower build
 
 php app/console assets:install --symlink
 php app/console assetic:dump
