63b7debe7cf8a50904c1a6ded567581c86c2fbd4
Ruud Klaver <r.klaver@bureaublauwgeel.nl>
2018-04-05 16:40:24 +0200

    Update console command path in shell tools

--- shell-tools/create_migration.sh
+++ shell-tools/create_migration.sh
@@ -3,10 +3,10 @@
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -rf ${SCRIPT_PATH}/../web/uploads &&
-php ${SCRIPT_PATH}/../app/console doctrine:database:drop --force --if-exists &&
-php ${SCRIPT_PATH}/../app/console doctrine:database:create &&
-php ${SCRIPT_PATH}/../app/console doctrine:migrations:migrate --no-interaction --allow-no-migration &&
-php ${SCRIPT_PATH}/../app/console doctrine:migrations:diff --no-interaction &&
-php ${SCRIPT_PATH}/../app/console doctrine:migrations:migrate --no-interaction --allow-no-migration &&
-php ${SCRIPT_PATH}/../app/console doctrine:fixtures:load --no-interaction
-# php ${SCRIPT_PATH}/../app/console kuma:search:populate full
+php ${SCRIPT_PATH}/../bin/console doctrine:database:drop --force --if-exists &&
+php ${SCRIPT_PATH}/../bin/console doctrine:database:create &&
+php ${SCRIPT_PATH}/../bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration &&
+php ${SCRIPT_PATH}/../bin/console doctrine:migrations:diff --no-interaction &&
+php ${SCRIPT_PATH}/../bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration &&
+php ${SCRIPT_PATH}/../bin/console doctrine:fixtures:load --no-interaction
+# php ${SCRIPT_PATH}/../bin/console kuma:search:populate full
